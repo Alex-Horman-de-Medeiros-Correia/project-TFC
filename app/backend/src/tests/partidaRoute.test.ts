5be0dad127dda6b4fee2aab60bc087e1c7d8c145
Alex-Horman-de-Medeiros-Correia <alexhorman1990@gmail.com>
2022-11-05 06:11:51 -0300

    testes - ajustando

--- app/backend/src/tests/partidaRoute.test.ts
+++ app/backend/src/tests/partidaRoute.test.ts
@@ -1,4 +1,4 @@
-import chai from 'chai';
+import * as chai from 'chai';
 
 import { expect } from 'chai';
 // @ts-ignore
@@ -10,22 +10,22 @@ chai.use(chaiHttp);
 
 
   describe('Quando a requisição tem sucesso', () => {
-    it(' status 200', async () => {
+    it('status 200', async () => {
       const httResponse = await chai.request(app).get('/matches')
 
       expect(httResponse.status).to.equal(200)
     })
-    it(' status 201', async () => {
+    it('status 201', async () => {
       const httResponse = await chai.request(app).post('/matches')
 
       expect(httResponse.status).to.equal(201)
     })
-    it(' status 200', async () => {
+    it('status 200', async () => {
       const httResponse = await chai.request(app).patch('/matches/:id/finish')
 
       expect(httResponse.status).to.equal(200)
     })
-    it(' status 200', async () => {
+    it('status 200', async () => {
       const httResponse = await chai.request(app).patch('/matches/:id')
       
       expect(httResponse.status).to.equal(200)
